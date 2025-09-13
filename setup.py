bfa657b7a7d605fb7728603644edde364b2c8103
Angeal13 <45517931+Angeal13@users.noreply.github.com>
2025-09-13 18:38:44 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bus-tracking-system",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "pygame>=2.5.2",
+        "pyttsx3>=2.90",
+        "gTTS>=2.3.2",
+        "boto3>=1.34.62",
+        "mariadb>=1.1.8",
+        "pytz>=2023.3.post1",
+        "keyboard>=0.13.5",
+        "requests>=2.31.0"
+    ],
+    entry_points={
+        'console_scripts': [
+            'bus-system=src.main:main',
+        ],
+    },
+)
